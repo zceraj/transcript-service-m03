9bf3d677009d252cc8b4d47bd4be21e54229f7a3
Patrick Zhang <pzhang345@gmail.com>
2026-09-17 15:58:25 -0400

    added mutation 1

--- src/additional.spec.ts
+++ src/additional.spec.ts
@@ -1,12 +1,25 @@
 // Group Members: Patrick, Zara, Sage 
 
+import { beforeEach, describe, expect, it } from "vitest";
+import { TranscriptDB, type TranscriptService }  from "./transcript.service.ts";
+
+let db: TranscriptService;
+
+beforeEach(() => {
+  db = new TranscriptDB();
+});
 // Classify the surviving mutants into innocuous (didn’t introduce a bug) 
 // and non-innocuous (did introduce a bug). 
 // Note: Student IDs should be positive.
 
-// Mutation 1: Line 40 - non-innocuous - Patrick
+// Mutation 1: Line 40 - non-innocuous
+describe('addStudent', () => {
+    it("Student IDs returns should be positive", () => {
+        expect(db.addStudent("test student")).toBeGreaterThan(0);
+    })
+})
 
-// Mutation 2: Line 43 - innocuous
+// Mutation 2: Line 43 - innocuous 
 
 // Mutation 3: Line 53 - non-innocuous
 
