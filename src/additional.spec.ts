fa5b3f6a56735b87e700b284a0358aed6cbf9d04
zceraj <167219401+zceraj@users.noreply.github.com>
2026-09-17 15:43:45 -0400

    should show additional spec now

---
+++ src/additional.spec.ts
@@ -0,0 +1,23 @@
+// Group Members: Patrick, Zara, Sage 
+
+// Classify the surviving mutants into innocuous (didn’t introduce a bug) 
+// and non-innocuous (did introduce a bug). 
+// Note: Student IDs should be positive.
+
+// Mutation 1: Line 40 - non-innocuous 
+
+// Mutation 2: Line 43 - innocuous
+
+// Mutation 3: Line 53 - non-innocuous
+
+// Mutation 4: Line 54 - non- innocuous
+
+// Mutation 5: Line 70 - innocuous
+
+// Mutation 6: Line 121 - non-innocuous
+
+// Mutation 7: Line 125 - innocuous
+
+// 4 tests needed to kill all non-innocuous mutants.
+
+
